6eb9e90a503ff96060353619ff435891612342ad
Domth13 <domth13@gmail.com>
2023-10-30 13:39:31 +0100

    added info text

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ import motor.motor_asyncio
 # Get the MongoDB connection string from an environment variable
 MONGODB_CONNECTION_STRING = os.environ.get("MONGODB_CONNECTION_STRING")
 SECURITY_CODE = os.environ.get("SECURITY_CODE")
+USERNAME_INFO = """
+Please use the following infos to create your username (use lowercase letters and write them in the same sequence without whitespace between.) \n
+1. First letter of the first name of your mother (or the most important female reference person in your childhood):\n
+2. First letter of the first name of your father (or the most important male reference person in your childhood):\n
+3. First letter of your birthplace:\n
+4. Birth month of your mother as number (or the most important female reference person in your childhood):\n
+5. Birth year of your father (in this format yyyy) (or the most important male reference person in your childhood):\n
+
+Example: hag061960
+"""
 
 async def fetch_user_passwords():
     try:
@@ -25,6 +35,8 @@ async def fetch_user_passwords():
 
 async def main():
     st.title("OM Password App")
+
+    st.markdown(USERNAME_INFO)
 
     username = st.text_input("Enter your username:")
     security_code = st.text_input("Enter your security code:")
